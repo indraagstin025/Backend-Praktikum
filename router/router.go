04042a2bbfde9697f694cc82473994fdf43875de
indraagstin025 <indraagustin012@gmail.com>
2025-06-30 14:55:06 +0700

    perubahan

--- router/router.go
+++ router/router.go
@@ -8,14 +8,14 @@ import (
 
 func SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
-	api.Get("/mahasiswa", middleware.Middlewares("admin"), handler.GetAllMahasiswa)
+	api.Get("/mahasiswa", handler.GetAllMahasiswa)
 
 	api.Get("/", handler.Homepage)
-	api.Get("/mahasiswa", handler.GetAllMahasiswa)
+	api.Get("/mahasiswa",  handler.GetAllMahasiswa)
 	api.Get("/mahasiswa/:npm", handler.GetAllMahasiswaByNPM)
-	api.Post("/mahasiswa", handler.CreateMahasiswa)     // POST untuk tambah mahasiswa
-	api.Put("/mahasiswa/:npm", handler.UpdateMahasiswa)  // PUT untuk update mahasiswa
-	api.Delete("/mahasiswa/:npm", handler.DeleteMahasiswa)
+	api.Post("/mahasiswa", middleware.Middlewares("admin"), handler.CreateMahasiswa)     // POST untuk tambah mahasiswa
+	api.Put("/mahasiswa/:npm", middleware.Middlewares("admin"), handler.UpdateMahasiswa)  // PUT untuk update mahasiswa
+	api.Delete("/mahasiswa/:npm", middleware.Middlewares("admin"), handler.DeleteMahasiswa)
 
 	
 	app.Post("/register", handler.Register)

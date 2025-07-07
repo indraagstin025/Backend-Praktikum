39bdff5bd96995819d7bb3fe232405751bce4c38
indraagstin025 <indraagustin012@gmail.com>
2025-07-07 14:21:10 +0700

    push 1

--- config/cors.go
+++ config/cors.go
@@ -3,7 +3,7 @@ package config
 var allowedOrigins = []string{
 	"https://indrariksa.github.io/",
 	"http://localhost:5173/",
-	"https://frontend-praktikum-nu.vercel.app/"
+	"https://frontend-praktikum-nu.vercel.app/",
 }
 
 func GetAllowedOrigins() []string {
